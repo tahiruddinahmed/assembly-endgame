4fff05f55723b8d1e19e013c5d1ece9d550390e4
tahiruddinahmed <01tahirahmed@gmail.com>
2026-09-08 13:34:29 +0530

    Project Setup

---
+++ README.md
@@ -0,0 +1,2 @@
+Assembly EndGame (React)
+- Tahir Uddin Ahmed
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-slate-800 text-4xl font-extrabold text-center flex h-[100vh] justify-center items-center">Start Coding!</h1>  
+}
+
+export default App
