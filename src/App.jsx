63cf8f8025bad2941a767f3d0621e0782ba4cbe5
tahiruddinahmed <01tahirahmed@gmail.com>
2026-09-10 17:24:33 +0530

    step 2: desgining the winning status card, later we will make it dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 function App() {
   return (
     <>
-      <header className="h-[144px] w-full flex flex-col gap-[20px]">
-        <div className="flex flex-col gap-[4px]">
-          <h1 className="text-[20px] font-medium text-white text-center">Assembly: EndGame</h1>
-          <p className="text-center text-[14px] text-[#8E8E8E]">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      <header className="h-[144px] w-full flex flex-col gap-[20px] items-center">
+        <div className="flex flex-col gap-[4px] items-center">
+          <h1 className="text-[20px] font-medium text-[#F9F4DA] text-center">Assembly: EndGame</h1>
+          <p className="text-[14px] max-w-[350px] text-center text-[#8E8E8E]">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </div>
 
+       {/* Status section */}
+       <div className="px-[6px] py-[6px] w-[352px] bg-[#10A95B] rounded-[4px] flex flex-col items-center justify-center">
+          <h2 className="text-[20px] text-[#F9F4DA]">You Win!</h2>
+          <p className="text-[16px] text-[#F9F4DA]">Well done! 🎉</p>
+       </div>
       </header>
     </>
   ) 
--- steps.md
+++ steps.md
@@ -1,5 +1,14 @@
 
-  ## Goal: Build out the main parts of our app
+ 
+  ## STEP 1: Build out the main parts of our app
   
   - Challenge: Add a header with the game title
   and description. Startin' out easy 🙂🚶‍➡️
+
+## Step 2: Build out the main parts of our app
+ 
+ - Challenge: Build a status section below the header.
+  For now, you can just hard-code in the styles for
+  a winning game, and we'll make it more dynamic
+  later.
+ 
